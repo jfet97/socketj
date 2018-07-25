317a9f4fc73646a33ab824dde03abf1c160ee253
jfet97 <andrysimo1997@gmail.com>
2018-07-25 15:58:57 +0200

    safdas

---
+++ node.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
+app.use(cors());
+
+
+// App setup
+const app = express();
+// Server setup for socket
+const server = app.listen(5000, () => console.log('listening to requests on port 5000'));
+
+// Static files: middleware che serve sempre pagina html cercandola nella pagina public
+
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+// triggered when someone connects, 'socket' is a reference to the socket opened with a client
+// each client owns his socket
+io.on('connection', (socket) => {
+    console.log("made socket connection", socket.id);
+
+    // when this particular socket receive something
+    // i'm waiting a message: name of the message is 'chat', data is the data into a chat message
+    socket.on('chat', (data) => {
+        // message received will be sent back to all clients, also to the one that has sent it because it must display it as well
+        // io.sockets: all sockets connected
+        io.sockets.emit('chat-response', data);
+        console.log(data);
+    })
+
+
+    socket.on('typing', (data) => {
+        // data will be sant back to all sockets except the one that has sent it
+        socket.broadcast.emit('typing-broadcast', data);
+        console.log(data);
+    })
+});
+
+
---
+++ public/chat.js
@@ -0,0 +1,99 @@
+// Make connection
+   const socket = io.connect('localhost:5000');
+
+
+// Query DOM
+const 
+    message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+// Emit events
+btn.addEventListener('click', () => {
+    // message name, message data
+    if((!message.value)||(!handle.value)) return;
+    socket.emit('chat', {
+        "message": message.value,
+        "handle": handle.value
+    });
+
+
+});
+
+
+/*
+message.addEventListener('keypress', () => {
+    // message name, message data
+    if(!handle.value) return;
+    socket.emit('typing', {
+        nickname: handle.value,
+        emptyMessage: message.value ? false : true // emptyMessage is true if the message is empty
+    });
+});*/
+
+message.addEventListener('input', () => {
+    // message name, message data
+    if(!handle.value) return;
+    socket.emit('typing', {
+        nickname: handle.value,
+        emptyMessage: message.value ? false : true // emptyMessage is true if the message is empty
+    });
+});
+
+
+
+// Listen for events
+
+socket.on('chat-response', (data) => {
+
+    // remove always the typing messages because now a valid message is arrived
+    console.log(feedback);
+    console.log(feedback.firstChild);
+    while (feedback.firstChild) {
+        feedback.removeChild(feedback.firstChild);
+    }
+
+    // console.log(data);
+    let p = document.createElement("p");
+    let strong = document.createElement("strong");
+    let nickname = document.createTextNode(data.handle + ": ");
+    let message = document.createTextNode(data.message);
+
+    strong.appendChild(nickname);
+    p.appendChild(strong);
+    p.appendChild(message);
+
+    output.appendChild(p);
+});
+
+
+socket.on('typing-broadcast', (data) => {
+
+    let feedbackBabies = feedback.children;
+    console.log("Questi sono i bimbi di feedback: ", feedbackBabies);
+    for(let i = 0; i < feedbackBabies.length; i++) {
+         let typedMessage = feedbackBabies[i].firstChild.innerText;
+        // console.log(typedMessage);
+        let writer = typedMessage.substring(0, typedMessage.indexOf(" "));
+        if(writer === data.nickname) feedbackBabies[i].remove();
+         
+
+    }
+    
+    
+    // if data.message is null I'm not going to display anything
+    if(data.emptyMessage) return;
+
+    let p = document.createElement("p");
+    let em = document.createElement("em");
+    let text = document.createTextNode(data.nickname + " is typing a message...");
+
+    
+    em.appendChild(text);
+    p.appendChild(em);
+    feedback.appendChild(p);
+    // console.log("Questo è feedback: ", feedback);
+   
+});
